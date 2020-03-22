a1eb5a89d4b9347075dcf5f2eb907416408cd0fa
John Neigel <johnfneigel@gmail.com>
2020-03-22 09:05:32 -0700

    fix project structure

---
+++ Android/InHome/app/src/main/java/com/japps/inhome/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.japps.inhome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReminderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
