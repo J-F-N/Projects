8c47e7948245def11bc67ba9c62fd395086806e8
JohnNeigel <johnfneigel@gmail.com>
2020-03-21 21:35:55 -0700

    work on UI for Reminders

--- Android/app/src/main/java/com/japps/inhome/MainActivity.kt
+++ Android/app/src/main/java/com/japps/inhome/MainActivity.kt
@@ -3,7 +3,7 @@ package com.japps.inhome
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ReminderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
