080fb95cd2fb9aec1544ff43e5d5a3b90724fdf3
JohnNeigel <johnfneigel@gmail.com>
2020-03-24 22:22:17 -0700

    added fragment for new reminder activity.

--- Android/InHome/app/src/main/java/com/japps/inhome/MainActivity.kt
+++ Android/InHome/app/src/main/java/com/japps/inhome/MainActivity.kt
@@ -2,22 +2,12 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
  class ReminderActivity : AppCompatActivity() {
-
-    private lateinit var recyclerView : RecyclerView
-     private lateinit var viewAdapter : RecyclerView.Adapter<>
-     private lateinit var viewManager : RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminder)
 
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = CardAdapter(cardData)
-
-        recyclerView = findViewById<RecyclerView>(R.id.cardRecycler)
     }
 }
