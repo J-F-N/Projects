7a66025cb23769053b60497d39c1b060e470e628
J-F-N <58239384+J-F-N@users.noreply.github.com>
2020-07-25 17:42:20 -0700

    Fix and Implement
    Fixed calendar crashing.
    Implemented date range picking.

--- Android/InHome/app/src/main/java/com/example/inhome/FragmentDate.java
+++ Android/InHome/app/src/main/java/com/example/inhome/FragmentDate.java
@@ -8,45 +8,48 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.squareup.timessquare.CalendarPickerView;
+
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Objects;
-
 
 public class FragmentDate extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       /* Date today = new Date();
+
         Calendar nextYear = Calendar.getInstance();
-        nextYear.add(Calendar.YEAR,1);
+        nextYear.add(Calendar.YEAR, 1);
 
-        CalendarPickerView datePicker = (CalendarPickerView) Objects.requireNonNull(getView()).findViewById(R.id.calendar);
-        datePicker.init(today, nextYear.getTime()).withSelectedDate(today);
+        View view = inflater.inflate(R.layout.fragment_date, container, false);
+        CalendarPickerView calendar = (CalendarPickerView) view.findViewById(R.id.calendar);
+        Date today = new Date();
+        calendar.init(today, nextYear.getTime())
+                .inMode(CalendarPickerView.SelectionMode.RANGE)
+                .withSelectedDate(today);
 
-        Date dateSelected = datePicker.getSelectedDate(); //date selected
+        calendar.getSelectedDates();
 
-        datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+        calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
             @Override
             public void onDateSelected(Date date) {
 
                 Calendar calSelected = Calendar.getInstance();
                 calSelected.setTime(date);
 
-                String selectedDate = "" + calSelected.get(Calendar.DAY_OF_MONTH)
-                        + "/" + calSelected.get(Calendar.MONTH)
-                        + "/" + calSelected.get(Calendar.YEAR);
+                String selectedDate = "" + calSelected.get(Calendar.DAY_OF_MONTH);
+                String selectedMonth = "" + calSelected.get(Calendar.MONTH);
+                String selectedYear = "" + calSelected.get(Calendar.YEAR);
 
-                Toast.makeText(FragmentDate.super.getContext(), selectedDate, Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onDateUnselected(Date date) {
 
             }
-        });*/
+        });
 
-        return (ViewGroup) inflater.inflate(R.layout.fragment_date, container, false);
+        return view;
     }
 
     public static FragmentDate newInstance() {
