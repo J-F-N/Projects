18a32286b340c6dedddd14c896d44fc6991e2ca9
J-F-N <58239384+J-F-N@users.noreply.github.com>
2020-10-27 10:38:27 -0700

    Project init
    Android Studio project creation and dependencies added

---
+++ Android/InHome/app/src/main/java/com/NCodes/inhome/AlarmListActivity.java
@@ -0,0 +1,14 @@
+package com.NCodes.inhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_list);
+    }
+}
